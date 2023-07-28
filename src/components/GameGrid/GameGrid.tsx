b4c61c3978dea10b8709e7bf84eb80b6a8889251
Andres Moraga <moraguexdev@gmail.com>
2023-07-27 20:37:58 -0600

    creates a generic data fetching hook

--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -1,11 +1,11 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
-import useGames from '../../hooks/useGames';
 import GameCard from '../GameCard';
 import GameCardSkeleton from '../GameCardSkeleton';
 import GameCardContainer from '../GameCardContainer';
+import useGames from '../../hooks/useGames';
 
 const GameGrid = () => {
-	const { games, error, isLoading } = useGames();
+	const { data, error, isLoading } = useGames();
 	const skeletons = [1, 2, 3, 4, 5, 6];
 
 	return (
@@ -19,7 +19,7 @@ const GameGrid = () => {
 						</GameCardContainer>
 					))}
 
-				{games.map((game) => (
+				{data.map((game) => (
 					<GameCardContainer>
 						<GameCard key={game.id} game={game} />
 					</GameCardContainer>
